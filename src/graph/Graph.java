a8db4ea61a0563369e2db4273447e3cf419d1329
Abhinav Manchanda <abhinav.manchanda@gmail.com>
2016-05-04 01:06:54 +0530

    Find if the graph contains an element
    - Create a graph.
    
    - Find for one neighbour.
    - Find should return false for Disconnected.
    - Multiple neighbours.
    - Indirect Connections.
    - Bidirectional.

--- src/graph/Graph.java
+++ src/graph/Graph.java
@@ -1,4 +1,35 @@
 package graph;
 
+import java.util.*;
+
 public class Graph {
+  private String name;
+  private List<Graph> neighbours;
+
+  public Graph(String name) {
+    this.name = name;
+    neighbours = new ArrayList<Graph>();
+  }
+
+  public void addNeighbour(Graph neighbour) {
+    this.neighbours.add(neighbour);
+    neighbour.neighbours.add(this);
+  }
+
+  public boolean isConnectedTo(Graph other) {
+    Set<Graph> visited = new HashSet<Graph>();
+    LinkedList<Graph> pending = new LinkedList<Graph>();
+    if(this == other) return true;
+    pending.addAll(neighbours);
+    visited.add(this);
+    while(pending.peek() != null){
+      Graph element = pending.poll();
+      if(element.equals(other)) return true;
+      visited.add(element);
+      for (Graph neighbour : element.neighbours) {
+        if(!visited.contains(neighbour)) pending.add(neighbour);
+      }
+    }
+    return false;
+  }
 }
--- test/graph/GraphTest.java
+++ test/graph/GraphTest.java
@@ -1,10 +1,56 @@
 package graph;
 
+import org.junit.Before;
 import org.junit.Test;
 
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
 public class GraphTest {
+
+  private Graph graph;
+  private Graph second;
+
+  @Before
+  public void setup(){
+    graph = new Graph("first");
+    second = new Graph("second");
+    graph.addNeighbour(second);
+  }
+
   @Test
-  public void shouldRun(){
-    Graph graph = new Graph();
+  public void shouldFindNeighbour(){
+    assertTrue(graph.isConnectedTo(second));
+  }
+
+  @Test
+  public void shouldNotFindDisconnectedNode(){
+    Graph third = new Graph("third");
+    assertFalse(graph.isConnectedTo(third));
+  }
+
+  @Test
+  public void shouldFindMultipleNeighbours(){
+    Graph third = new Graph("third");
+    graph.addNeighbour(third);
+    assertTrue(graph.isConnectedTo(second));
+    assertTrue(graph.isConnectedTo(third));
+  }
+
+  @Test
+  public void shouldFindIndirectConnections(){
+    Graph third = new Graph("third");
+    Graph fourth = new Graph("fourth");
+    Graph fifth = new Graph("fifth");
+    graph.addNeighbour(third);
+    second.addNeighbour(fourth);
+    third.addNeighbour(fifth);
+    assertTrue(graph.isConnectedTo(fourth));
+    assertTrue(graph.isConnectedTo(fifth));
+  }
+
+  @Test
+  public void shouldBeBidirectional(){
+    assertTrue(second.isConnectedTo(graph));
   }
 }
